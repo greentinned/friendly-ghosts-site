45dd4fb38895dbf8dc74236863ea13b6f41a2cc5
Sergey K <hello.to@me.com>
2021-12-05 11:14:54 +0300

    gh-pages url fix

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -2,9 +2,7 @@ import NextImage, { ImageProps, ImageLoaderProps } from 'next/image'
 
 const loader = (props: ImageLoaderProps) => {
   const { src, width, quality } = props
-  return `https://greentinned.github.io/friendly-ghosts-site/${src}?w=${width}&q=${
-    quality || 75
-  }`
+  return `https://greentinned.github.io/${src}?w=${width}&q=${quality || 75}`
 }
 
 const Image = (props: ImageProps) => {
