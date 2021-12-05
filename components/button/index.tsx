93adac871cdf644ad07077c92874832d17de6cc7
Sergey K <hello.to@me.com>
2021-12-05 11:14:52 +0300

    wide button mod

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -3,13 +3,14 @@ import styles from './Button.module.css'
 export interface ButtonProps {
   title: string
   subtitle?: string
+  wide?: boolean
   style?: string
   onRelease(): void
 }
 
 const Button = (props: ButtonProps) => {
-  const { title, subtitle, style, onRelease } = props
-  const cn = `${styles.main} ${style || ''}`.trim()
+  const { title, subtitle, wide, style, onRelease } = props
+  const cn = `${styles.main} ${wide ? styles.wide : ''} ${style || ''}`.trim()
 
   return (
     <div className={cn} onClick={onRelease}>
