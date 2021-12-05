406d0368f54fed5f0fbd4ba87542caaaa193a374
Sergey K <hello.to@me.com>
2021-12-05 11:14:51 +0300

    add text styles

--- components/index.tsx
+++ components/index.tsx
@@ -5,5 +5,20 @@ import WalletButton from './wallet_button'
 import Logo from './logo'
 import SocialButton from './social_button'
 import Footer from './footer'
+import { Hero, Heading1, Heading2, Heading3, Paragraph, Caption } from './text'
 
-export { Header, Button, MintButton, WalletButton, Logo, SocialButton, Footer }
+export {
+  Header,
+  Button,
+  MintButton,
+  WalletButton,
+  Logo,
+  SocialButton,
+  Footer,
+  Hero,
+  Heading1,
+  Heading2,
+  Heading3,
+  Paragraph,
+  Caption,
+}
---
+++ components/text/index.tsx
@@ -0,0 +1,66 @@
+import { FC } from 'react'
+import textStyles from './Text.module.css'
+
+export interface TextProps {
+  styles?: Array<string>
+}
+
+const safeConcatStyles = (
+  style: string,
+  styles?: Array<string>
+): Array<string> => [style, ...(styles ? styles : [])]
+
+export const Text: FC<TextProps> = (props) => {
+  const { styles, children } = props
+  const cn = `${textStyles.main} ${styles ? styles.join(' ') : ''}`.trim()
+
+  return <span className={cn}>{children}</span>
+}
+
+export const Hero: FC<TextProps> = (props) => {
+  return (
+    <Text styles={safeConcatStyles(textStyles.hero, props.styles)}>
+      {props.children}
+    </Text>
+  )
+}
+
+export const Heading1: FC<TextProps> = (props) => {
+  return (
+    <Text styles={safeConcatStyles(textStyles.h1, props.styles)}>
+      {props.children}
+    </Text>
+  )
+}
+
+export const Heading2: FC<TextProps> = (props) => {
+  return (
+    <Text styles={safeConcatStyles(textStyles.h2, props.styles)}>
+      {props.children}
+    </Text>
+  )
+}
+
+export const Heading3: FC<TextProps> = (props) => {
+  return (
+    <Text styles={safeConcatStyles(textStyles.h3, props.styles)}>
+      {props.children}
+    </Text>
+  )
+}
+
+export const Paragraph: FC<TextProps> = (props) => {
+  return (
+    <Text styles={safeConcatStyles(textStyles.p, props.styles)}>
+      {props.children}
+    </Text>
+  )
+}
+
+export const Caption: FC<TextProps> = (props) => {
+  return (
+    <Text styles={safeConcatStyles(textStyles.c, props.styles)}>
+      {props.children}
+    </Text>
+  )
+}
