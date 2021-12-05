e0073052eea761d482183043c00af6b7d7c0d4b7
Sergey K <hello.to@me.com>
2021-12-05 11:14:54 +0300

    gh-pages url fix

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -1,8 +1,11 @@
 import NextImage, { ImageProps, ImageLoaderProps } from 'next/image'
 
+const basePath =
+  process.env.NODE_ENV === 'production' ? 'https://greentinned.github.io' : './'
+
 const loader = (props: ImageLoaderProps) => {
   const { src, width, quality } = props
-  return `https://greentinned.github.io/${src}?w=${width}&q=${quality || 75}`
+  return `${basePath}${src}?w=${width}&q=${quality || 75}`
 }
 
 const Image = (props: ImageProps) => {
