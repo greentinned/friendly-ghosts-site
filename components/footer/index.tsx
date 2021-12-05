1192162a4805fbdcb339c50bc8882da8313f7af8
Sergey K <hello.to@me.com>
2021-12-05 11:14:50 +0300

    footer with text styles

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Paragraph, Caption } from '../text'
 import styles from './Footer.module.css'
 import letteringSrc from './img/lettering.svg'
 
@@ -6,9 +7,9 @@ const Footer = () => {
   return (
     <footer className={styles.main}>
       <Image src={letteringSrc} alt="Even Ghosts Deserve Love" />
-      <div className={styles.copy}>
+      <Paragraph styles={[styles.copy]}>
         <span className={styles.year}>2021</span> friendly ghosts
-      </div>
+      </Paragraph>
     </footer>
   )
 }
