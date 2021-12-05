a44b78616dea7679bc7fad65325904fce7c52286
Sergey K <hello.to@me.com>
2021-12-05 11:14:58 +0300

    footer fix

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,12 +1,23 @@
 import styles from './Footer.module.css'
-import letteringSrc from './img/lettering.svg'
+import letteringSrc from './img/lettering_clouds.svg'
 import { Paragraph } from '../text'
 import Image from '../image'
 
 const Footer = () => {
   return (
     <footer className={styles.main}>
-      <Image src={letteringSrc} alt="Even Ghosts Deserve Love" />
+      <div className={styles.content}>
+        <div className={styles.lettering} />
+      </div>
+      {/* <div className={styles.content}>
+        <div className={styles.contentInner}>
+          <Image
+            src={letteringSrc}
+            alt="Even Ghosts Deserve Love"
+            layout="fill"
+          />
+        </div>
+      </div> */}
       <Paragraph styles={[styles.copy]}>
         <span className={styles.year}>2021</span> friendly ghosts
       </Paragraph>
