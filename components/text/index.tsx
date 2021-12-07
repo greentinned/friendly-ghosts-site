b2f51e20cc006de6b0cb5d4eb35325bf3fdd96ab
Sergey K <hello.to@me.com>
2021-12-07 20:14:41 +0300

    add invert mod to Text component

--- components/text/index.tsx
+++ components/text/index.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import textStyles from './Text.module.css'
 
 export interface TextProps {
+    invert?: boolean;
     styles?: Array<string>
 }
 
@@ -11,55 +12,61 @@ const safeConcatStyles = (
 ): Array<string> => [style, ...(styles ? styles : [])]
 
 export const Text: FC<TextProps> = (props) => {
-    const { styles, children } = props
-    const cn = `${textStyles.main} ${styles ? styles.join(' ') : ''}`.trim()
+    const { invert, styles, children } = props
+    const cn = `${textStyles.main} ${invert ? textStyles.invert : ''} ${styles ? styles.join(' ') : ''}`.trim()
 
     return <span className={cn}>{children}</span>
 }
 
 export const Hero: FC<TextProps> = (props) => {
+    const { styles, ...restProps } = props
     return (
-        <Text styles={safeConcatStyles(textStyles.hero, props.styles)}>
+        <Text styles={safeConcatStyles(textStyles.hero, props.styles)} {...restProps}>
             {props.children}
         </Text>
     )
 }
 
 export const Heading1: FC<TextProps> = (props) => {
+    const { styles, ...restProps } = props
     return (
-        <Text styles={safeConcatStyles(textStyles.h1, props.styles)}>
+        <Text styles={safeConcatStyles(textStyles.h1, props.styles)} {...restProps}>
             {props.children}
         </Text>
     )
 }
 
 export const Heading2: FC<TextProps> = (props) => {
+    const { styles, ...restProps } = props
     return (
-        <Text styles={safeConcatStyles(textStyles.h2, props.styles)}>
+        <Text styles={safeConcatStyles(textStyles.h2, props.styles)} {...restProps}>
             {props.children}
         </Text>
     )
 }
 
 export const Heading3: FC<TextProps> = (props) => {
+    const { styles, ...restProps } = props
     return (
-        <Text styles={safeConcatStyles(textStyles.h3, props.styles)}>
+        <Text styles={safeConcatStyles(textStyles.h3, props.styles)} {...restProps}>
             {props.children}
         </Text>
     )
 }
 
 export const Paragraph: FC<TextProps> = (props) => {
+    const { styles, ...restProps } = props
     return (
-        <Text styles={safeConcatStyles(textStyles.p, props.styles)}>
+        <Text styles={safeConcatStyles(textStyles.p, props.styles)} {...restProps}>
             {props.children}
         </Text>
     )
 }
 
 export const Caption: FC<TextProps> = (props) => {
+    const { styles, ...restProps } = props
     return (
-        <Text styles={safeConcatStyles(textStyles.c, props.styles)}>
+        <Text styles={safeConcatStyles(textStyles.c, props.styles)} {...restProps}>
             {props.children}
         </Text>
     )
