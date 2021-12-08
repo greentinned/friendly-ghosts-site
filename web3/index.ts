62b99e50f5b04eb7c55c6c70bb186d5e1e664989
Sergey K <hello.to@me.com>
2021-12-08 05:46:49 +0300

    mint

--- web3/index.ts
+++ web3/index.ts
@@ -3,7 +3,7 @@ import { sleep } from '../helpers'
 
 export async function walletFetcher(): Promise<any> {
     // TODO: Should throw if error
-    await sleep(5000)
+    await sleep(3000)
     return Promise.resolve('0xF00...BAR')
 }
 
@@ -18,7 +18,19 @@ export async function mintPriceFetcher(_url: string): Promise<any> {
 }
 
 export async function mintFetcher(_url: string, amount: number): Promise<any> {
+    await sleep(3000)
+
     // TODO: Should throw if error
-    await sleep(5000)
-    return Promise.resolve(new Array(amount).map(() => '../pages/home/img/honoraries/1.jpeg'))
+    // https://swr.vercel.app/docs/error-handling
+    if (false) {
+        const error = new Error('An error occurred while fetching the data.')
+        throw error
+    }
+
+    const data = []
+    for (let i = 1; i <= amount; i++) {
+        data.push('https://friendlyghosts.xyz//_next/static/media/17.331e9a80.jpeg?w=1200&q=75')
+    }
+
+    return Promise.resolve(data)
 }
